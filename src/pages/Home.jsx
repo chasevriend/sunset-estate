efea15ec7d290c67a8c2e1a03d386a320f5dfc2d
NEWUSER <chasevriend84@gmail.com>
2023-04-30 14:37:29 -0700

    new commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import Nav from '../components/Nav'
+import React from 'react';
+import Nav from '../components/Nav';
+import Form from '../components/Form';
+import About from '../components/About';
+import Hero from '../components/Hero';
 
 const Home = () => {
   return (
     <>
         <Nav />
+        <Form />
+        <Hero />
+        <About />
+        {/* <About /> */}
+        {/* Newest Deals */}
+        {/* Footer */}
     </>
   )
 }
